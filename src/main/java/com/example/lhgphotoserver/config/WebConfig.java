b7102118266839fe78761d5289ace84dd526b854
이효규 <lhg961006@gmail.com>
2024-01-08 13:53:18 +0900

    // 클라이언트 도메인 변경 (집 로컬)

--- src/main/java/com/example/lhgphotoserver/config/WebConfig.java
+++ src/main/java/com/example/lhgphotoserver/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**") // 엔드포인트 패턴을 필요에 따라 조정
-                .allowedOriginPatterns("http://61.80.148.189:3000") // 클라이언트 도메인
+                .allowedOriginPatterns("http://192.168.0.201/3000") // 클라이언트 도메인
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("Content-Type", "Authorization")
                 .allowCredentials(true);
